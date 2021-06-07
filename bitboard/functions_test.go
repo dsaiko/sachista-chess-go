e8c605b95199b1e76dd72991b1aa975dbed85826
Dušan Saiko <dusan@saiko.cz>
2021-06-08 01:20:18 +0200

    Adding BitPop

---
+++ bitboard/functions_test.go
@@ -0,0 +1,17 @@
+package bitboard
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBitPop(t *testing.T) {
+	b := Universe
+
+	count := 64
+	for i := 0; i < 64; i++ {
+		assert.Equal(t, count, b.PopCount())
+		assert.Equal(t, i, b.BitPop())
+		count--
+	}
+}
