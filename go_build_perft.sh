e7f5ac8549df34a0a95ba1887d8d344f7eaed84e
Dušan Saiko <dusan@saiko.cz>
2021-08-20 01:21:16 +0200

    Adding dissassembly build step.

--- go_build_perft.sh
+++ go_build_perft.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 go build -o bin/sachista-chess-perft perft/main.go
+objdump -D bin/sachista-chess-perft > bin/sachista-chess-perft.objdump
 
 env GOOS=windows GOARCH=amd64 go build -o bin/sachista-chess-perft.exe perft/main.go
